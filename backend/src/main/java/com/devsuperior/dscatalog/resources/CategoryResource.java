615034c97dd4e9a18e2c6a6915d322fc9a63e2ec
Otávio Landim <otavio.landim@gmail.com>
2022-09-16 00:41:09 -0300

    Organize imports

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,15 +1,14 @@
 package com.devsuperior.dscatalog.resources;
 
 import com.devsuperior.dscatalog.entities.Category;
-
 import com.devsuperior.dscatalog.services.CategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
