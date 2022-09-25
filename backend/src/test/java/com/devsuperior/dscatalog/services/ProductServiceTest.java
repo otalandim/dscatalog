4a3c373cce4c617eb3a9728a2ed753dccdb664ff
Otávio Landim <otavio.landim@gmail.com>
2022-09-24 21:29:42 -0300

    Add test shouldThrowDBExceptionWhenIdDoesNotExist

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTest.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTest.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscatalog.services;
 
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -24,13 +26,17 @@ public class ProductServiceTest {
 
     private long existingId;
     private long noExistingId;
+    private long dependId;
 
     @BeforeEach
     public void setup() {
         existingId = 1L;
+        noExistingId = 1000L;
+        dependId = 4L;
 
         Mockito.doNothing().when(repository).deleteById(existingId);
         Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(noExistingId);
+        Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependId);
     }
 
     @Test
@@ -49,5 +55,14 @@ public class ProductServiceTest {
         });
 
         Mockito.verify(repository, Mockito.times(1)).deleteById(noExistingId);
+    }
+
+    @Test
+    public void deleteShouldThrowDatabaseExceptionWhenIdDoesNotExist () {
+        Assertions.assertThrows(DatabaseException.class, () -> {
+            service.delete(dependId);
+        });
+
+        Mockito.verify(repository, Mockito.times(1)).deleteById(dependId);
     }
 }
