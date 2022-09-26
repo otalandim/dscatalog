108fb8d04ff1c9d1364bfff19e6b0cf226ad9af2
Otávio Landim <otavio.landim@gmail.com>
2022-09-26 18:35:37 -0300

    Adding test findAllShouldReturnPageZeroSizeZero

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIT.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIT.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog.services;
 
+import com.devsuperior.dscatalog.dto.ProductDTO;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
@@ -7,8 +8,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 public class ProductServiceIT {
 
     @Autowired
@@ -39,5 +44,15 @@ public class ProductServiceIT {
         Assertions.assertThrows(ResourceNotFoundException.class, () -> {
             service.findById(noExistingId);
         });
+    }
+
+    @Test
+    public void findAllPagedShouldReturnPageWhenPageZeroSizeDez() {
+        PageRequest pageRequest = PageRequest.of(0,10);
+        Page<ProductDTO> result = service.findAllPaged(pageRequest);
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals(0, result.getNumber());
+        Assertions.assertEquals(10, result.getSize());
+        Assertions.assertEquals(countTotalProducts, result.getTotalElements());
     }
 }
