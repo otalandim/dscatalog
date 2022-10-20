9a210e240b9195f9de4deb174e2feaafccd422a5
Otávio Landim <otavio.landim@gmail.com>
2022-10-20 14:00:37 -0300

    Adding user entity

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Role.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Role.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 
 public class Role implements Serializable {
     private static final long serialVersionUID = 1L;
-    
+
     private Long id;
     private String authority;
 
