64447d72c2dd947b0251d7fcac048caf59a4da4a
Otávio Landim <otavio.landim@gmail.com>
2022-10-24 13:43:23 -0300

    Adding role repository and user service

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/UserService.java
@@ -0,0 +1,88 @@
+package com.devsuperior.dscatalog.services;
+
+import com.devsuperior.dscatalog.dto.RoleDTO;
+import com.devsuperior.dscatalog.dto.UserDTO;
+import com.devsuperior.dscatalog.entities.Role;
+import com.devsuperior.dscatalog.entities.User;
+import com.devsuperior.dscatalog.repositories.RoleRepository;
+import com.devsuperior.dscatalog.repositories.UserRepository;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    private static final String USER_NOT_FOUND = "User not found";
+    private static final String INTERNAL_SERVER_ERROR = "Internal server error";
+
+    @Transactional(readOnly = true)
+    public Page<UserDTO> findAllPaged(Pageable pageable){
+        Page<User> list = repository.findAll(pageable);
+        return list.map(x -> new UserDTO(x));
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        User user = obj.orElseThrow(() -> new ResourceNotFoundException(USER_NOT_FOUND));
+        return new UserDTO(user);
+    }
+
+    @Transactional
+    public UserDTO create(UserDTO userDto) {
+        User user = new User();
+        copyDtoToEntity(userDto, user);
+        user = repository.save(user);
+        return new UserDTO(user);
+    }
+
+    @Transactional
+    public UserDTO update(Long id, UserDTO userDto) {
+        try {
+            User user = repository.getOne(id);
+            copyDtoToEntity(userDto, user);
+            user = repository.save(user);
+            return new UserDTO(user);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(USER_NOT_FOUND);
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(USER_NOT_FOUND);
+        } catch (DataIntegrityViolationException d) {
+            throw new DatabaseException(INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private void copyDtoToEntity(UserDTO userDto, User user) {
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        user.setEmail(userDto.getEmail());
+        user.getRoles().clear();
+
+        for (RoleDTO roleDto : userDto.getRoles()) {
+            Role role = roleRepository.getOne(roleDto.getId());
+            user.getRoles().add(role);
+        }
+    }
+}
