620403629a607e9eb5cedc0909f5d929c1bdbfc5
Otavio Landim <otavio.landim@gmail.com>
2025-10-17 10:29:42 -0300

    adding repository and service category

---
+++ backend/src/main/java/com/oivato/dscatalog/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.oivato.dscatalog.repositories;
+
+import com.oivato.dscatalog.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
