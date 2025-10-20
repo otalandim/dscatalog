a1a972cb443aba322f46939643f6e0df1864d764
Otavio Landim <otavio.landim@gmail.com>
2025-10-20 10:11:21 -0300

    remove import

--- backend/src/main/java/com/oivato/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/oivato/dscatalog/controllers/CategoryController.java
@@ -1,13 +1,11 @@
 package com.oivato.dscatalog.controllers;
 
 import com.oivato.dscatalog.dtos.CategoryDTO;
-import com.oivato.dscatalog.entities.Category;
 import com.oivato.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
